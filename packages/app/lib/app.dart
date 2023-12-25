66ebebe8857ec63508bf772f69624fd754e885ee
コタロー <kk.nnkktr@gmail.com>
2023-12-25 12:57:00 +0900

    アプリのテーマをuiパッケージに移管

--- packages/app/lib/app.dart
+++ packages/app/lib/app.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:octopus_viewer/ui/router/router.dart';
-import 'package:octopus_viewer/ui/theme.dart';
+import 'package:ui/theme/octopus_viewer_theme.dart';
 
 class App extends ConsumerWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final theme = ref.watch(themeProvider);
+    final theme = ref.watch(octopusViewerThemeProvider);
     final router = ref.watch(routerProvider);
 
     return MaterialApp.router(
